b6b822ab29bcab8cfb3c045ada1a09bb92a405b6
Yerong Li <yerongli.ads@outlook.com>
2022-04-01 05:22:17 -0500

    Script for remove duplicate items

--- duplicate.py
+++ duplicate.py
@@ -7,6 +7,6 @@ df = pd.read_csv(args.file)
 df.url = df.url.apply(lambda x : "/".join(x.split('/')[:6]))
 df.drop_duplicates(['url'], inplace=True)
 url = df.url[0]
-print(url)
-url = df.url[1]
+print(df.url)
+# url = df.url[1]
 # print(url)
