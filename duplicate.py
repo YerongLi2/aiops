782f63366f2815ac29265581dd6888d2b2e0245e
Yerong Li <yerongli.ads@outlook.com>
2022-04-01 05:12:36 -0500

    Script for remove duplicate items

--- duplicate.py
+++ duplicate.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--file", 'Filename')
+parser.add_argument("--file", help = 'Filename')
 args = parser.parse_args()
 print('args.file', args.file)
 df = pd.read_csv(args.file)
