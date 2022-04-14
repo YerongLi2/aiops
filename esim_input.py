ae00b26f60f95fdd08613aaca734c753b0a704e9
Yerong Li <yerong.li@outlook.com>
2022-04-14 15:36:25 -0500

    Add filename

--- esim_input.py
+++ esim_input.py
@@ -12,7 +12,7 @@ args = parser.parse_args()
 # df = pd.read_csv(args.file)
 for filename in filenames:
     df = pd.read_csv(filename)
-    print(eval(df['key'].values))
+    print(len(df['key'].values))
     # data = [eval(item) for item in ].tolist()
     data = list(itertools.chain(*data))
     print(data[0])
