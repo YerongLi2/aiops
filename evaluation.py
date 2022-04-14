1ef5454c801984e71c33f86b46c460ee6a2cab38
Yerong Li <yerong.li@outlook.com>
2022-04-14 17:46:10 -0500

    Updaste shop4

--- evaluation.py
+++ evaluation.py
@@ -35,10 +35,8 @@ fdup = open('duplicates.txt', 'r')
 for line in fdup:
     if len(line.split(',')) == 2:
         new, old = line.split(',')
-        if 0 == len(df.get(df['url']==new).key.values.tolist()) :
-            print(old)
-            continue
+        # if 0 == len(df.get(df['url']==new).key.values.tolist()) :
         new_posts = df.get(df['url']==new).key.values.tolist()[0]
-        # old_posts = df.get(df['url']==old).key.values.tolist()[0]
-        # print(new_posts)
-        # print(old_posts)
+        old_posts = df.get(df['url']==old).key.values.tolist()[0]
+        print(new_posts)
+        print(old_posts)
