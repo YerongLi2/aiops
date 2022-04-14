31a777d277132f8415a6603c7f852884dc2c540b
Yerong Li <yerong.li@outlook.com>
2022-04-14 14:09:50 -0500

    Print keywords

--- extraction_version.py
+++ extraction_version.py
@@ -10,4 +10,5 @@ parser.add_argument("--file", help = 'Filename')
 
 args = parser.parse_args()
 df = pd.read_csv(args.file)
-keywords = [post[0] for post in df.iloc[1]['key']] 
+keywords = [post[0] for post in df.iloc[1]['key']]
+print(keywords)
