badabb5edd346c337adf6bd92ec93901bc70ccd7
Yerong Li <yerong.li@outlook.com>
2022-04-14 05:05:19 -0500

    data parameter must be a list

--- extraction_ml.py
+++ extraction_ml.py
@@ -19,8 +19,7 @@ if 'key' not in df.columns:
 
 try:
     for i in tqdm.tqdm(range(df.shape[0])):
-        print(df.iloc[i]['key'] != 0)
-        if df.iloc[i]['key'] != '0': continue
+        if df.iloc[i]['key'] != 0: continue
         posts = eval(df.iloc[i]['posts'])
         for j in range(len(posts)):
             
