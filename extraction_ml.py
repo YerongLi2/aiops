7bad189d36097c06df042584e8959642fe238ce1
Yerong Li <yerong.li@outlook.com>
2022-04-14 04:27:29 -0500

    monkeylearn

--- extraction_ml.py
+++ extraction_ml.py
@@ -18,7 +18,7 @@ if 'key' not in df.columns:
     df['key'] = ['']*df.shape[0]
 
 for i in range(df.shape[0]):
-    print(df.iloc['post'])
+    print(df.iloc[i]['post'])
     sys.exit()
     # result = ml.extractors.extract(model_id, data)
     # print(result.body)
